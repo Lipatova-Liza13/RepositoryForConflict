b1d09e7c1f5a29fb28de9d31a6fc7b8a05e6adc7
Lipatova-Liza13 <lipatovaliza13@gmail.com>
2020-08-20 14:38:21 +0300

    change appDelegate, add arr =[2,3]

--- projectConflict/projectConflict/AppDelegate.swift
+++ projectConflict/projectConflict/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+let arr = [2,3]
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
